0e78bd7fae4510461d2e1c37b4d81a13dd9dcbfe
IustinCostinNeaga <iustincostinneaga@gmail.com>
2024-10-11 14:00:16 +0200

    implemented move button

--- MoveButton.cs
+++ MoveButton.cs
@@ -2,9 +2,15 @@ using Godot;
 using System;
 
 public partial class MoveButton : TextureButton {
-    private Move move;
+    private Move move = new Move();
+    private Label name;
+
+    public override void _Ready() {
+        name = GetNode<Label>("Name");
+        name.Text = move.name;
+    }
 
     private void OnClick() {
-        
+        GD.Print("Move used");
     }
 }
--- Scripts/Pokemons/Move.cs
+++ Scripts/Pokemons/Move.cs
@@ -3,11 +3,18 @@ using Godot.Collections;
 
 [GlobalClass]
 public partial class Move : Resource {
-    [Export] private string name;
+    [Export] public string name;
     [Export] private string description;
     [Export] private Priority priority;
     [Export] private Array<MoveCommand> moves;
 
+    public Move() {
+        this.name = "MissingMove";
+        this.description = "";
+        this.priority = Priority.Normal;
+        this.moves = new Array<MoveCommand>();
+    }
+    
     internal enum Priority {
         Absolute,
         Faster,
